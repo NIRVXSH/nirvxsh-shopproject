3b5e70627c6f7e8265fedaf2684d083591729c46
NIRVXSH <setthanan2544@gmail.com>
2023-06-17 09:36:50 +0700

    connect database

--- pkg/databases/db.go
+++ pkg/databases/db.go
@@ -1,1 +1,18 @@
 package databases
+
+import (
+	"log"
+
+	"github.com/NIRVXSH/NIRVXSH-shop-project/config"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+func DbConnect(cfg config.IDbConfig) *sqlx.DB {
+	db, err := sqlx.Connect("pgx", cfg.Url())
+	if err != nil {
+		log.Fatalf("connect to db failed %v\n", err)
+	}
+	db.DB.SetMaxOpenConns(cfg.MaxOpenConns())
+	return db
+}
